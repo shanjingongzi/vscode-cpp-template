c14818260222dbc6eb577d73a3f30c43769f538e
shanjingongzi <cenqiuming@sandboxol.com>
2024-08-05 11:33:40 +0800

    initialized

---
+++ cmake/Tools.cmake
@@ -0,0 +1,59 @@
+function(CollectDirectories result cur_dir)
+    file(GLOB children RELATIVE ${cur_dir} ${cur_dir}/*)
+    set(subdirs "")
+    if(IS_DIRECTORY ${cur_dir})
+        list(APPEND subdirs ${cur_dir}/${child})
+    endif()
+    foreach(child ${children})
+        if(IS_DIRECTORY ${cur_dir}/${child}) 
+            string(TOLOWER ${child} low_child) 
+            if(NOT low_child STREQUAL "build" )
+                if(NOT low_child STREQUAL "cmakefiles")
+                    list(APPEND subdirs ${cur_dir}/${child})
+                    CollectDirectories(subsubdirs ${cur_dir}/${child})
+                    list(APPEND subdirs ${subsubdirs})
+                endif()
+            endif()
+        endif()
+    endforeach()
+    set(${result} ${subdirs} PARENT_SCOPE)
+endfunction()
+
+function(CollectSources RESULT ALL_DIRECTORIES BASE_DIR)
+    set(SRCS "")
+    foreach(DIR  IN LISTS ALL_DIRECTORIES)
+        file(GLOB CURRENT_SOURCES
+            ${DIR}/*.h
+            ${DIR}/*.hpp
+            ${DIR}/*.inl
+            ${DIR}/*.cpp
+            ${DIR}/*.c
+            ${DIR}/*.cxx
+        )
+        if(CURRENT_SOURCES)
+            list(APPEND SRCS ${CURRENT_SOURCES})
+            file(RELATIVE_PATH GROUP_NAME ${BASE_DIR} ${DIR})
+            source_group(${GROUP_NAME} FILES ${CURRENT_SOURCES})
+        endif()
+    endforeach()
+    set(${RESULT} ${SRCS} PARENT_SCOPE)
+endfunction()
+
+
+function(InstallBinaries  target dir)
+install(
+    TARGETS ${target}
+    RUNTIME DESTINATION ${dir}
+)
+endfunction()
+
+function(InstallLibraries target dir)
+    install(
+        TARGETS ${target}
+        ARCHIVE DESTINATION ${dir}         
+        LIBRARY DESTINATION ${dir}         
+    ) 
+endfunction()
+
+function(InstallHeaders dir)
+endfunction()
